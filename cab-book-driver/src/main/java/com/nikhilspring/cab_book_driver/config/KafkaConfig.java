12ee38c4eafef7f03d0ae2e8d64572e9c93fdd27
Venkata Nikhil Chakravarthy Korrapati <nikekorrapati@gmail.com>
2025-06-25 23:08:57 -0400

    Kafka-based cab booking app

---
+++ cab-book-driver/src/main/java/com/nikhilspring/cab_book_driver/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.nikhilspring.cab_book_driver.config;
+
+import com.nikhilspring.cab_book_driver.constant.AppConstant;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder
+                .name(AppConstant.CAB_LOCATION)
+                .build();
+    }
+}
